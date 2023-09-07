4ec337e792acb156d6e8444deb70a985db094baf
Heisen-erg <heisenberg.database.1@gmail.com>
2023-09-07 19:59:27 +0530

    hh

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const Register = require("./Register.js")
 const plans = require("./routes.js")
 
-app.use(cors({origin: "https://followers.heisenberg.in.net/"})) 
+app.use(cors({origin: "https://followers.heisenberg.in.net"})) 
 app.use("/plans", plans)
 app.use("/user",Register)
 app.get("/",(req,res)=>{res.json({message:"Server Is Working"})})
