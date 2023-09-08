7cb1eeef6d452859d9efa9c8e7494c6699beb618
Heisen-erg <heisenberg.database.1@gmail.com>
2023-09-08 19:07:01 +0530

    hh

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ xyz.create(req.body).then(()=>{res.json({message:"Successfully Registered"})})
 app.get("/getallusers",async (req,res)=>{
     await mongoose.connect("mongodb+srv://heisenbergdatabase1:uDVtMXlxymOMO8eX@cluster0.vwlkprw.mongodb.net/").then(async ()=>{
     
-         xyz.find({}).then((response)=>{  res.json({allusers : response })})
+    const allusers = await xyz.find({})
+    res.json({ allusers})
       
   
 })
