1555c2a635106a512ab485dabef6ac212cdbfe7b
Heisen-erg <heisenberg.database.1@gmail.com>
2023-09-08 17:23:30 +0530

    hh

--- index.js
+++ index.js
@@ -9,4 +9,12 @@ app.use(cors({origin: "https://followers.heisenberg.in.net"}))
 app.use("/plans", plans)
 
 app.get("/",(req,res)=>{res.json({message:"Server Is Workingcheck2"})})
+app.post("/registering", async (req,res)=>{
+    await mongoose.connect("mongodb+srv://heisenbergdatabase1:uDVtMXlxymOMO8eX@cluster0.vwlkprw.mongodb.net/")
+  
+    
+await UserName.create(req.body.Rishi).then(()=>{res.json({message:"Successfully Registered"})})
+
+
+})
 app.listen(4000)
--- routes.js
+++ routes.js
@@ -4,9 +4,7 @@ const app1 = express()
 const Plan = require("./model")
 const mongoose = require("mongoose")
 const UserName = require("./usermodel")
-const cors = require("cors")
 
-app1.use(cors({origin: "https://followers.heisenberg.in.net"}))
 app1.use(express.json())
 // app1.use(function(req, res, next) {
 //     res.set("Access-Control-Allow-Origin", 'https://followers.heisenberg.in.net');
@@ -46,14 +44,7 @@ await Plan.deleteMany({ planname: drop })
    
   
 })
-app1.post("/registering", async (req,res)=>{
-    await mongoose.connect("mongodb+srv://heisenbergdatabase1:uDVtMXlxymOMO8eX@cluster0.vwlkprw.mongodb.net/")
-  
-    
-await UserName.create(req.body.Rishi).then(()=>{res.json({message:"Successfully Registered"})})
 
-
-})
 
 
 
